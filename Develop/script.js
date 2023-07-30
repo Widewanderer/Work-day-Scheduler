1c6abb0fec72e293975993eb4d6f530a5fa372a7
Tyson Trotz <slavatrotz@gmail.com>
2023-07-30 18:20:56 -0500

    add Day.js clock at top, added code to save input on first time slot 9:00, creating loop over array of rowIds to set and pull from local storage

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,42 @@
 
 $(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
+  // create array here  ,,, using strings define ids being used, loop over array,, run all code though ,,, string interpralation
+  var rowId = [
+    "hour-9",
+    "hour-10",
+    "hour-11",
+    "hour-12",
+    "hour-13",
+    "hour-14",
+    "hour-15",
+    "hour-16",
+    "hour-17",
+  ];
+
+  for (let i = 0; i < rowId.length; i++) {
+    var input = document.querySelector("#${rowId[i]}.description");
+    var appointment = localStorage.getItem("rowId[i]"); // fill getItem() with ids from array
+    if (appointment) {
+      input.value = appointment;
+    }
+  }
+  var saveButton = document.querySelector(".saveBtn");
+  var saveInput = saveButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    var inputValue = input.value;
+    localStorage.setItem("rowId[i]", inputValue); // match first argument with variable appointment
+  });
+
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  // var hourBlock = parseInt($(this).attr('id').split('-')[1])
+  //console.log(hourBlock)
+
+  //if( currentHour ===
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -21,6 +52,15 @@ $(document).ready(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-  const dayjs = dayjs();
-  console.log(dayjs.format("dddd, MMMM D, YYYY h:mm A"));
+
+  var currentHour = dayjs().hour();
+  console.log(currentHour);
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY H:mm A"));
 });
+
+// var saveImput = $(".saveBtn").on("click", function () {
+//   // Use jQuery to select the textarea and retrieve its value
+//   const textValue = $(".description").val();
+//   // Save the value (You can replace this with your own saving logic)
+//   console.log("Text value:", textValue);
+// });
